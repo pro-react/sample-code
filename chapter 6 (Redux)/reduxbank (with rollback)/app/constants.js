14f824727885d3d5fd6fa332772f2ad342c4b5ad
Cássio Zen <cassiozen@gmail.com>
2016-05-26 12:04:15 -0300

    Removed wrong version of reduxbank example

--- chapter 6 (Redux)/reduxbank (with rollback)/app/constants.js
+++
@@ -1,6 +0,0 @@
-export default {
-  WITHDRAW_FROM_ACCOUNT: 'withdraw from account',
-  DEPOSIT_INTO_ACCOUNT: 'deposit into account',
-  TOGGLE_EXCHANGE: 'toggle exchange',
-  ROLLBACK: 'rollback operation',
-};
